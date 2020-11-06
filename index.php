4da8f6bf59208d588690e5e582331c1739fa1b53
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-06 18:41:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello BlackPink</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body style="background-color:#FCA2A2;">
+    <p></p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="img/58ab9c745dd1e415d52534d685c4d4b8.jpg"  >
+                    <div class="card-body">
+                        <h4 class="card-title">Rosé Blackpink</h4>
+                        <p>โรเซ่(Rosé) นักร้อง, นักเต้น วงBlackpink </p>
+                        <p>สัญชาติ : นิวซีเเลนด์-ออสเตรเลีย-เกาหลีใต้ </p>
+                    </div>
+                    <div class="card-footer">
+                        ที่มา https://sites.google.com/site/blackpingfan/prawati-ro-se
+                    </div>
+                </div>
+            </div>
+        <div class="col">
+            <div class="card">
+                <img class="card-img-top" src="img/BLACKPINK-Official-Logo-HD.jpg">
+                <div class="card-body">
+                    <h4 class="card-title">Blackpink</h4>
+                    <p>วงเกิร์ลกรุ๊ปเกาหลีชื่อดังระดับโลกจากค่าย YG Entertainment</p>
+                    <p>ประกอบด้วยสมาชิกจำนวน 4 คนคือ Jisoo (จีซู), Jennie (เจนนี), Rosé (โรเซ) และ Lisa (ลิซ่า)</p>
+                </div>
+                <div class="card-footer">
+                    ที่มา https://music.trueid.net/detail/v2KEyXG10Eaw
+                </div>
+            </div>
+        </div>
+        <div class="col">
+        <div class="card">
+                <img class="card-img-top" src="img/79de0a18dd8a0713ca6d1c8255489464.jpg" >
+                <div class="card-body">
+                    <h4 class="card-title">Lisa Blackpink</h4>
+                    <p> Lisa (ลิซ่า) ลลิษา มโนบาล นักร้อง, นักเต้น วงBlackpink</p>
+                    <p>สัญชาติ : ไทย /โด้งดังระดับโลก :)</p>
+                </div>
+                <div class="card-footer">
+                    ที่มา https://sites.google.com/site/worapolljn/home/prawati-swn-taw-khxng-lisa
+                </div>
+            </div>
+        </div>
+</body>
+</html>
